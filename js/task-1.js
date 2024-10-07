019b6d14eb57e5cb8783331efb9f0699f000486f
AnnaAr <ann.artyuh90@gmail.com>
2024-10-07 20:21:04 +0300

    new style

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ const categories = document.querySelectorAll(`#categories .item`);
 categories.forEach(category => {
   const categoriesTitle = category.querySelector(`h2`).textContent;
   const elementsItem = category.querySelectorAll('ul li').length;
-});
 
-console.log(`Category: ${categoriesTitle}`);
-console.log(`Elements: ${elementsItem}`);
+  console.log(`Category: ${categoriesTitle}`);
+  console.log(`Elements: ${elementsItem}`);
+});
